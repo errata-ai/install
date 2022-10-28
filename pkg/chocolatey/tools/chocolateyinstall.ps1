b91d00eb86f589dce7ed71dc123545c0b07bb71a
jdkato <joseph@jdkato.io>
2022-10-28 11:37:29 -0700

    Bump Vale to 2.21.0

--- pkg/chocolatey/tools/chocolateyinstall.ps1
+++ pkg/chocolatey/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = 'Vale'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.20.2/vale_2.20.2_Windows_64-bit.zip'
+$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.21.0/vale_2.21.0_Windows_64-bit.zip'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url64bit      = $url64
 
-  checksum64      = 'e2583620585df6090c658a54df2895372f8a0e3f56934ae4a754d7be1c085171'
+  checksum64      = 'e8ff9c74e7771837f3704316c30ee267585224dec97c975cdb29c9ac9dfbb359'
   checksumType64  = 'sha256'
 
 }
